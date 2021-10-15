c63fb72225d48d2fc64cfe0313083b6c4a0e8e5e
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-10-15 09:00:24 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class Position(Enum):
+    Goalie = 1,
+    Defence = 2,
+    Forward = 3
+
+class Player:
+    def __init__(self, name: str, pos: Position):
+        self._name = name    
+        self._pos = pos
+
+
+p = Player("Foppa", Position.Forward) 
+p2 = Player("Sudden", Position.Forward) 
+
+def printplayer(namn: str, age: int, position: Position) :
+    if position == Position.Forward:
+        print("Anfallare")
+    if position == Position.Defence:
+        print("Försvarare")
+    if position == Position.Goalie:
+        print("Målis")
+    print(namn + "," + age)
+
+printplayer("Mats Sundin", 50, Position.Forward )
